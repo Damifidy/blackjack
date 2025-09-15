2592404f2bf4c4b602e05efd4467a7290ae012bd
Damifidy <82963489+Damifidy@users.noreply.github.com>
2025-09-15 18:34:38 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,23 @@
 // Starting game board values
 var cardsInDeck;
+
 $("#dev-menu").hide();
+
+function launchab() {
+    const tab = window.open('about:blank', '_blank');
+    const iframe = tab.document.createElement('iframe');
+    const stl = iframe.style;
+    stl.border = stl.outline = 'none';
+    stl.width = '100vw';
+    stl.height = '100vh';
+    stl.position = 'fixed';
+    stl.left = stl.right = stl.top = stl.bottom = '0';
+    iframe.src = self.location;
+    tab.document.body.appendChild(iframe);
+    window.parent.window.location.replace('about:blank');
+}
+
+launchab();
 
 function addChips(amount) {
 	currentChipBalance = parseInt(currentChipBalance, 10) + amount;
